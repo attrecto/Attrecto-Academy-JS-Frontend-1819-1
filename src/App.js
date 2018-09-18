5da7d0d9a12b23314e0d990a1615904f220fc94f
Martin Horváth <martin.horvath@attrecto.com>
2018-09-18 11:31:07 +0200

    Introduce Component State

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import { Button } from './components';
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  increaseCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decreaseCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
     return (
       <div>
         <div>Welcome to Attrecto Academy</div>
-        <Button>Button</Button>
+        <div>Counter: {this.state.counter}</div>
+        <Button onClick={this.increaseCounter}>Increase</Button>
+        <Button styleType="secondary" onClick={this.decreaseCounter}>
+          Decrease
+        </Button>
       </div>
     );
   }
